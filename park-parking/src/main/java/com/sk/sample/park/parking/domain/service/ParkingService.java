79b1e535eb7e536aaee94fa577b24733bf298ff7
yegam805 <52989215+yegam805@users.noreply.github.com>
2019-07-24 16:57:21 +0900

    parking 삭제

--- park-parking/src/main/java/com/sk/sample/park/parking/domain/service/ParkingService.java
+++
@@ -1,6 +0,0 @@
-package com.sk.sample.park.parking.domain.service;
-
-public interface ParkingService {
-	void parkIn(String parkingLotId, String carNumber);
-	void parkOut(String parkingLotId, String carNumber);
-}
